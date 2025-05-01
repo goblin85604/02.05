a200e4e346c75e183dcca7272e410d8656f3e7a0
goblin <goblin02410@gmail.com>
2025-05-01 02:06:36 -0300

    chore: test player movement logic, remove unused assets, add ground node

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+var target_position: Vector2
 
+func _ready() -> void:
+	target_position = global_position
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		var horizontal_movement = Vector2(event.position).x
+		target_position.x = horizontal_movement
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction = (target_position - global_position).normalized()
+	if global_position.distance_to(target_position) > 2:
+		velocity = direction * SPEED
+		move_and_slide()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+		velocity = Vector2.ZERO
